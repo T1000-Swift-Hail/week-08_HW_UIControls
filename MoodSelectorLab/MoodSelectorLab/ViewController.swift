d4c3b0e8787996ea996b7621691cc6d38f39206f
Anas <Anas.h.Alhumaid@gmail.com>
2021-11-28 10:27:54 +0300

    Done

--- MoodSelectorLab/MoodSelectorLab/ViewController.swift
+++ MoodSelectorLab/MoodSelectorLab/ViewController.swift
@@ -8,16 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
     
-
+    @IBOutlet var viewLabel: UILabel!
+    
+    @IBOutlet var viewImage: UIImageView!
+    
+    @IBOutlet var controlImageLabel: MoodSelector!
+    
+    var moodsDescritionImage : [MoodPage] = [MoodPage(description: "Happy day make every one happy", imageName: "happy1"),MoodPage(description: "dont worry be happy for days", imageName: "sad1"),MoodPage(description: "Drink some water, and change your mood ", imageName: "angry1"),MoodPage(description: " have a good dream", imageName: "sleepy1")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        moodSet()
         
     }
-
+    
+    @IBAction func uicontrolView(_ sender: Any) {
+        viewImage.image = UIImage(named: moodsDescritionImage[controlImageLabel.selectedMoodIndex].imageName)
+        viewLabel.text = moodsDescritionImage[controlImageLabel.selectedMoodIndex].description
+        
+    }
+    
+    func moodSet(){
+        viewImage.image = UIImage(named: moodsDescritionImage[controlImageLabel.selectedMoodIndex].imageName)
+        viewLabel.text = moodsDescritionImage[controlImageLabel.selectedMoodIndex].description
+    }
     
 }
 
